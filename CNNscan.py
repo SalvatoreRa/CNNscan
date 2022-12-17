c8687417c580c5ea65d8c3a1f34ab8c1c7c4b855
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2022-12-17 18:53:52 +0100

    Update CNNscan.py

--- CNNscan.py
+++ CNNscan.py
@@ -17,7 +17,6 @@ import math
 def fetch_filters(layer = 0):
     model = torch.hub.load('pytorch/vision:v0.6.0', 'alexnet', pretrained=True)
     model.eval()
-    print('done downloading')
     idx_conv_layer = [0, 3, 6, 8, 10]
     filters = []
     for layer_idx in idx_conv_layer:
@@ -27,28 +26,33 @@ def fetch_filters(layer = 0):
     num_rows = 4
     num_cols = 4
     if layer == 0:
+      plt_dim = int(math.sqrt(16))
+      fig, axis =plt.subplots(plt_dim, plt_dim)
+      ax = axis.flatten()
       for i in range(16):
-        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
         npimg = np.array(t[i].numpy(), np.float32)
 
         npimg = (npimg - np.mean(npimg)) / np.std(npimg)
         npimg = np.minimum(1, np.maximum(0, (npimg + 0.5)))
         npimg = npimg.transpose((1, 2, 0))
-        ax1.imshow(npimg)
-        ax1.axis('off')
-        ax1.set_title(str(i))
-        ax1.set_xticklabels([])
-        ax1.set_yticklabels([]) 
+        ax[i].imshow(npimg)
+        ax[i].axis('off')
+        ax[i].set_title(str(i))
+        ax[i].set_xticklabels([])
+        ax[i].set_yticklabels([]) 
       plt.tight_layout()
       plt.show()
     else:
-      limit = 16
-      plt_dim = int(math.sqrt(limit))
-      for i, filt in enumerate(filters[layer].numpy()[:limit]):
-          plt.subplot(plt_dim, plt_dim, i+1)
-          plt.imshow(filt[0], cmap="gray")
-          plt.axis('off')
+      plt_dim = int(math.sqrt(16))
+      fig, axis =plt.subplots(plt_dim, plt_dim)
+      ax = axis.flatten()
+      for i in range(len(ax)):
+        ax[i].imshow(filters[layer].numpy()[i][0], cmap="gray")
+        ax[i].set_title(str(i))
+        ax[i].axis('off')
+      plt.tight_layout()
       plt.show()
+
 
 # Create the main app
 def main():
