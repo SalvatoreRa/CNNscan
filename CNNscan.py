2268ce1aae6ac5b60985e832f06cabe1dd1e3e37
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2022-12-18 13:18:41 +0100

    Update CNNscan.py

--- CNNscan.py
+++ CNNscan.py
@@ -21,6 +21,65 @@ def load_model():
     model.eval()
     return model
 
+def load_model():
+  model_urls = {
+    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-7be5be79.pth',
+  }
+  class AlexNet(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(AlexNet, self).__init__()
+        self.gradients = None
+        
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+        
+    def activations_hook(self, grad):
+        self.gradients = grad
+
+    def get_activations(self, x):
+        return self.features(x)
+    
+    def forward(self, x):
+        x = self.features(x)
+
+        hook = x.register_hook(self.activations_hook)
+        
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+  model = AlexNet()
+  state_dict = load_state_dict_from_url(model_urls['alexnet'],
+                                              progress=True)
+  model.load_state_dict(state_dict)
+  return model
+
+
 #Fetch filters
 def fetch_filters(model, layer = 0):
     
