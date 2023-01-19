d227420cc39cc537331ce5f9b68ffcb97d0243fc
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2023-01-19 22:41:22 +0100

    Update CNNscan.py

--- CNNscan.py
+++ CNNscan.py
@@ -1207,7 +1207,7 @@ def main():
         show_gradcam = st.button('show GradCam')
         if show_gradcam:
             t=11
-            heats, sup, act_map = GradCam(model, image_cam, t=11)
+            heats, sup, act_map = GradCam(model, image_cam, target_layer=t)
             cam_outputs(image_cam, heats, sup, act_map)
             
     with st.expander("Visualize Vanilla Backpropagation"):
