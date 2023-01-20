8563e844f777a6074af1abd89ab51d960163252c
Salvatore Raieli <32837577+SalvatoreRa@users.noreply.github.com>
2023-01-20 12:33:08 +0100

    Update CNNscan.py

--- CNNscan.py
+++ CNNscan.py
@@ -1175,7 +1175,7 @@ def main():
     
     st.sidebar.markdown("---")
     st.sidebar.header("Settings")
-    mod_app = st.selectbox('Select model for the app:',
+    mod_app = st.sidebar.selectbox('Select model for the app:',
             ('AlexaNET', 'VGG16', 'VGG19'))
     alexa_idx =[0, 3, 6, 8, 10]
     VGG16_filt = [0, 2, 5, 7, 10, 12, 14, 17, 19, 21, 24, 26, 28]
