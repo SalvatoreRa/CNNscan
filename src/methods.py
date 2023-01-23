c21287bbdc3d316fb4c9a5284614c55b9599bec3
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2023-01-23 21:24:59 +0100

    p

--- src/methods.py
+++ src/methods.py
@@ -20,8 +20,57 @@ from matplotlib import cm
 from torch.nn import ReLU
 from torch.autograd import Variable
 import torch.nn.functional as F
-from torch.optim import SGD
+from torch.optim import SGD, Adam
 import copy
 from matplotlib.colors import ListedColormap
 from copy import deepcopy
+import os
+import sys
+import pathlib
+
+
+##########################################################
+###########         Fetch Filters          ###############
+##########################################################
+
+
+def fetch_filters(model, idx_conv_layer = [0, 3, 6, 8, 10], layer = 0):
+    
+    
+    filters = []
+    for layer_idx in idx_conv_layer:
+        filters.append(model.features[layer_idx].weight.data)
+    t = filters[idx_conv_layer.index(layer)]
+    fig = plt.figure(figsize=(4,4))
+    num_rows = 4
+    num_cols = 4
+    if layer == 0:
+      plt_dim = int(math.sqrt(16))
+      fig, axis =plt.subplots(plt_dim, plt_dim)
+      ax = axis.flatten()
+      for i in range(16):
+        npimg = np.array(t[i].numpy(), np.float32)
+
+        npimg = (npimg - np.mean(npimg)) / np.std(npimg)
+        npimg = np.minimum(1, np.maximum(0, (npimg + 0.5)))
+        npimg = npimg.transpose((1, 2, 0))
+        ax[i].imshow(npimg)
+        ax[i].axis('off')
+        ax[i].set_title(str(i))
+        ax[i].set_xticklabels([])
+        ax[i].set_yticklabels([]) 
+      plt.tight_layout()
+      st.pyplot(fig)
+    else:
+      plt_dim = int(math.sqrt(16))
+      fig, axis =plt.subplots(plt_dim, plt_dim)
+      ax = axis.flatten()
+      for i in range(len(ax)):
+        ax[i].imshow(filters[idx_conv_layer.index(layer)].numpy()[i][0], cmap="gray")
+        ax[i].set_title(str(i))
+        ax[i].axis('off')
+      plt.tight_layout()
+      st.pyplot(fig)
+
+
 
