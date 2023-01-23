2ed252aa6fdc27c94528d70dc286994c1753fcbe
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2023-01-23 21:27:12 +0100

    Update CNNscan.py

--- CNNscan.py
+++ CNNscan.py
@@ -32,6 +32,7 @@ import pathlib
 #this to import modules
 sys.path.append(str(pathlib.Path().absolute()).split("/src")[0] + "/src")
 from utils import load_test_image, load_baseline
+from methods import fetch_filters
 
 @st.cache(ttl=12*3600)
 def load_model():
@@ -92,48 +93,7 @@ def load_model():
   model.load_state_dict(state_dict)
   return model
 
-##########################################################
-###########         Fetch Filters          ###############
-##########################################################
 
-
-def fetch_filters(model, idx_conv_layer = [0, 3, 6, 8, 10], layer = 0):
-    
-    
-    filters = []
-    for layer_idx in idx_conv_layer:
-        filters.append(model.features[layer_idx].weight.data)
-    t = filters[idx_conv_layer.index(layer)]
-    fig = plt.figure(figsize=(4,4))
-    num_rows = 4
-    num_cols = 4
-    if layer == 0:
-      plt_dim = int(math.sqrt(16))
-      fig, axis =plt.subplots(plt_dim, plt_dim)
-      ax = axis.flatten()
-      for i in range(16):
-        npimg = np.array(t[i].numpy(), np.float32)
-
-        npimg = (npimg - np.mean(npimg)) / np.std(npimg)
-        npimg = np.minimum(1, np.maximum(0, (npimg + 0.5)))
-        npimg = npimg.transpose((1, 2, 0))
-        ax[i].imshow(npimg)
-        ax[i].axis('off')
-        ax[i].set_title(str(i))
-        ax[i].set_xticklabels([])
-        ax[i].set_yticklabels([]) 
-      plt.tight_layout()
-      st.pyplot(fig)
-    else:
-      plt_dim = int(math.sqrt(16))
-      fig, axis =plt.subplots(plt_dim, plt_dim)
-      ax = axis.flatten()
-      for i in range(len(ax)):
-        ax[i].imshow(filters[idx_conv_layer.index(layer)].numpy()[i][0], cmap="gray")
-        ax[i].set_title(str(i))
-        ax[i].axis('off')
-      plt.tight_layout()
-      st.pyplot(fig)
 
 ##########################################################
 ###########         Feature maps           ###############
