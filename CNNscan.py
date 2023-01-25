fe76950a8c1e404546861dccd58ff2eb9363cfb5
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2023-01-25 20:34:16 +0100

    Update CNNscan.py

--- CNNscan.py
+++ CNNscan.py
@@ -40,9 +40,10 @@ from utils import (load_test_image, load_baseline,
 from methods import ( fetch_filters, advance_filt, fetch_feature_maps, CamExtractor, GradCam, Visualize_GradCam,
                     VanillaBackprop, VanillaBackprop_process, GuidedBackprop, GuidedBackprop_process, 
                     scoreCamExtractor, ScoreCam, scorecam_process, GuidedGradCam, gradient_gradcam,
-                    IntegratedGradients, integrated_gradient_process)
+                    IntegratedGradients, integrated_gradient_process,
+                    LRP, LRP_process)
 #from methods import ( 
-#    LRP, LRP_process, LayerCam, LayerCAM_process, , Grad_times_process, generate_smooth_grad, 
+#    , LayerCam, LayerCAM_process, , Grad_times_process, generate_smooth_grad, 
 #    smooth_grad_process, smooth_grad_process_guidBackprop, CNNLayerVisualization, visualise_layer_without_hooks, advance_filt)
 from outputs import cam_outputs, outputs_backprop, outputs_scorecam, \
     outputs_LRP, outputs_smoothgrad, output_adv_filt
