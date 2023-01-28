6f07b8bdae0563a004bf745a42b94a011fe2609c
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2023-01-28 10:31:50 +0100

    Update outputs.py

--- src/outputs.py
+++ src/outputs.py
@@ -219,7 +219,28 @@ def output_inverted(images, img):
         st.image(images[5])
         st.image(images[8])
 
+##########################################################
+###########    Class generated images      ###############
+##########################################################
 
+def outputs_CGI(images):
+    col1, col2, col3, col4 = st.columns([0.25, 0.25, 0.25, 0.25])
+    with col1:
+        st.image(images[0])
+        st.image(images[4])
+        st.image(images[8])
+    with col2:
+        st.image(images[1])
+        st.image(images[5])
+        st.image(images[9])
+    with col3:
+        st.image(images[2])
+        st.image(images[6])
+        
+    with col4:
+        st.image(images[3])
+        st.image(images[7])
+        
         
 ##########################################################
 ###########         Visualize DeepDream    ###############
