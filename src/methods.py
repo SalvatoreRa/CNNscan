72f65830a2928c7cac4ef67fd5f2e3ee8b477e60
SalvatoreRa <32837577+SalvatoreRa@users.noreply.github.com>
2023-03-02 21:53:06 +0100

    Update methods.py

--- src/methods.py
+++ src/methods.py
@@ -1311,3 +1311,59 @@ def dataframe_prediction(img, model):
   return df
 
 
+def lime(img, model, N_samples):
+  def get_pil_transform(): 
+    transf = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.CenterCrop(224)
+    ])    
+
+    return transf
+
+  def get_preprocess_transform():
+      normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                      std=[0.229, 0.224, 0.225])     
+      transf = transforms.Compose([
+          transforms.ToTensor(),
+          normalize
+      ])    
+
+      return transf    
+
+  pill_transf = get_pil_transform()
+  preprocess_transform = get_preprocess_transform()
+
+  def batch_predict(images):
+    model.eval()
+    batch = torch.stack(tuple(preprocess_transform(i) for i in images), dim=0)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    batch = batch.to(device)
+    
+    logits = model(batch)
+    probs = F.softmax(logits, dim=1)
+    return probs.detach().cpu().numpy()
+
+  test_pred = batch_predict([pill_transf(img)])
+  test_pred.squeeze().argmax()
+
+  explainer = lime_image.LimeImageExplainer()
+  explanation = explainer.explain_instance(np.array(pill_transf(img)), 
+                                          batch_predict, # classification function
+                                          top_labels=5, 
+                                          hide_color=0, 
+                                          num_samples= N_samples)   
+  
+  temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=5, hide_rest=False)
+  img_boundry1 = mark_boundaries(temp/255.0, mask)
+  img_boundry1 = Image.fromarray(np.uint8(img_boundry1*255))
+
+  temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10, hide_rest=False)
+  img_boundry2 = mark_boundaries(temp/255.0, mask)
+  img_boundry2 = Image.fromarray(np.uint8(img_boundry2*255))
+
+  return img_boundry1, img_boundry2
+  
+
+img_boundry1, img_boundry2 = lime(img, model, 1000)
